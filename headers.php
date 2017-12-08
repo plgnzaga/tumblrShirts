9ef1743bb6b7030e04e1655473a43c7645ad4637
Gonzaga <paul.s.t.gonzaga@accenture.com>
2017-12-08 20:08:17 +0800

    'initial-commit'

---
+++ headers.php
@@ -0,0 +1,16 @@
+<head>
+
+<title>Tumblr Shirts</title>
+<!--online
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+-->
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/mystyle.css">
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/thescript.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<?php include'headers.php';?>
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<body>
+
+	<nav class="lg-screen">
+		<ul>
+			
+			<li class="right">Men</li>
+			<li class="right">Women</li>
+			<li class="right">Kids</li>
+		</ul>
+
+	
+	</nav>
+
+
+
+
+</body>
+</html>
